063d4286559e83de6ae9dde0f004c6b1b34b5216
Jon Shea <jon@sheacloud.com>
2023-01-12 09:58:05 -0500

    Fix role definition names

--- azure/role-assignments/main.tf
+++ azure/role-assignments/main.tf
@@ -4,20 +4,20 @@ data "azurerm_subscription" "primary" {}
 resource "azurerm_role_assignment" "reader" {
   for_each             = toset(var.reader_principal_ids)
   scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "reader-role"
+  role_definition_name = "Reader"
   principal_id         = each.value
 }
 
 resource "azurerm_role_assignment" "contributor" {
   for_each             = toset(var.contributor_principal_ids)
   scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "contributor-role"
+  role_definition_name = "Contributor"
   principal_id         = each.value
 }
 
 resource "azurerm_role_assignment" "owner" {
   for_each             = toset(var.owner_principal_ids)
   scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "owner-role"
+  role_definition_name = "Owner"
   principal_id         = each.value
 }
